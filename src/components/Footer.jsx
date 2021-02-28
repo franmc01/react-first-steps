bf611c9a9ac3a6fed8e784559aac3148784b8bf5
francisco-1998 <francmarinc@gmail.com>
2021-02-28 12:53:15 -0500

    Entiendo los propTypes y los defaultProps

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 
 
 const Footer = ({anioActual}) => ( 
     <footer><p>Todos los derechos reservados &copy; Francisco Marin { anioActual }</p></footer> 
 );
  
+//Definicion de los propTypes, lo cuales sirven para indicar a otros desarrolladores que y cuales son las propiedades que recibira mi componente y son opcionales o requeridas para que funcione
+Footer.protoType = {
+    anioActual: PropTypes.number.isRequired
+}
+
+Footer.defaultProps = {
+    anioActual: 2022
+}
+
 export default Footer;
